11b80a67abea809f78e6dd532901a8c1e8e03b06
Laur017 <45920367+Laur017@users.noreply.github.com>
2022-03-14 14:49:36 +0200

    Add files via upload

---
+++ Lab3/Switch.java
@@ -0,0 +1,10 @@
+package com.example.Compulsory;
+public class Switch extends Node{
+
+public  Switch(String name, String macAddress,String location) {
+
+        this.setName(name);
+        this.setMacAddress(macAddress);
+        this.setLocation(location);
+
+}}
