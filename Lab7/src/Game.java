6276cad568fb0ff851e7dc4da008e8bbce40da9b
Laur017 <45920367+Laur017@users.noreply.github.com>
2022-04-18 15:54:19 +0300

    Add files via upload

--- Lab7/src/Game.java
+++ Lab7/src/Game.java
@@ -2,29 +2,13 @@ import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.List;
 
-public class Game {
+public class Game extends ThreadDeath{
 
     private final Bag bag = new Bag();
 
     private final Board board = new Board();
-    private final Dictionary dictionary = new MockDictionary();
+//    private final Dictionary dictionary = new Dictionary();
     private final List<Player> players = new ArrayList<>();
-    public void addPlayer(Player player) {
-        players.add(player);
-        player.setGame(this);
-    }
-    public void play() {
-        for (Player player : players) {
-           // start a new Thread representing the player;
-        }
-    }
-    public static void main(String args[]) {
-        Game game = new Game();
-        game.addPlayer(new Player("Player 1"));
-        game.addPlayer(new Player("Player 2"));
-        game.addPlayer(new Player("Player 3"));
-        game.play();
-    }
 
     public Bag getBag() {
         return bag;
@@ -34,5 +18,14 @@ public class Game {
         return board;
     }
 
+    public void addPlayer(Player player) {
+        players.add(player);
+        player.setGame(this);
+    }
+    public void play() {
+        for (Player p : players) {
+           new Thread(p).start();
+        }
+    }
 
 }
